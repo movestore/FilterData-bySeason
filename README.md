cb8cd6a359a3db4eff79940b6ffa5aa810ffd21d
andreakoelzsch <akoelzsch@ab.mpg.de>
2024-04-08 13:30:08 +0200

    update to move2 with improvements

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# Filter by Season
+# Filter/Annotate by Season
 MoveApps
 
 Github repository: *github.com/movestore/FilterData-bySeason*
 
 ## Description
-Selects positions that fall within a selected time range of several (also user-provided) years, i.e. spring migration of 2013 and 2014.
+Selects or annotate positions that fall within a selected time range of several (also user-provided) years, i.e. spring migration of 2013 and 2014.
 
 ## Documentation
-This App filters the data to defined seasonal time intervals of a selection of years. Of the user-defined start timestamp and end timestamp only month, year, hour, minute and second are extracted. Year(s) must independently specified or are all in the data set available years by default.
+This App annotates or filters the data with/to defined seasonal time intervals of a selection of years. Of the user-defined start timestamp and end timestamp only month, year, hour, minute and second are extracted. Year(s) must independently specified or are all in the data set available years by default.
 
 Note that the names of your new tracks (split by year) are composed of the animal ID (individual local identifier) and the year of the first location of the track.
 
 ### Input data
-moveStack in Movebank format
+move2 location object
 
 ### Output data
-moveStack in Movebank format
+move2 location object
 
 ### Artefacts
 none
@@ -23,9 +23,19 @@ none
 ### Settings
 **Start of your season (`startTimestamp`):** a timestamp with year, month, day, hour, minute and second has to be selected interactively. Year of this timestamp is not used for the analysis. Example: “2020-11-09 12:00:00”.
 
-**End of your season (UTC) (`endTimestamp`):** a timestamp with year, month, day, hour, minute and second has to be selected interactively. Year of this timestamp is not used for the analysis. Example: “2020-11-09 12:00:00”.
+**End of your season (UTC) (`endTimestamp`):** a timestamp with year, month, day, hour, minute and second has to be selected interactively. Year of this timestamp is not used for the analysis. Note that the starting year of the selected season is required if your specified season crosses December/January. Example: `2020-11-09 12:00:00`.
 
-**Years you want to select seasons from (`years`):** a string of comma-separated calender years. The default value is `ALL` which leads to the selection of all in the data available years. Example: 2013, 2014, 2015.
+**Years you want to select seasons from (`years`):** a string of comma-separated calender years. The default value is `ALL` which leads to the selection of all in the data available years. Example: 2013, 2014, 2015. 
+
+**Name of your season for annotation (`season`):** a conclusive name for the selected season. This will be attributed to all locations during that season in the new column 'season'. If you select not to filter the data, locations that are not in the selected season obtain the column entry `none`. If left empty this defaults to the specified time interval, e.g. `11-24 12:00:00 to 1-31 20:00:00`.
+
+**Filter data for season locations? (`filter`):** selection, if the user wants the input data to be filtered to only contain locations in the specified seasons (and years). If unselected, the input data set will be returned with an extra column 'season' appended. Default TRUE.
+
+**Split tracks by season? (`splitt`):** selection if the user wants to split the (animal/deployment) tracks by season. That way unrealistic steps connecting the end and start locations of different years within the same animal/deployment will be avoided, which can lead to wrong conclusions in subsequent analyses. Note that this setting is only possible if the above Filter setting is TRUE. Default FALSE.
+
+
+### Most common errors
+none so far for this move2 App, please add as issues here.
 
 ### Null or error handling:
 **Setting `startTimestamp`:** If the start timestamp or the end timestamp are not selected (i.e. NULL) the data is not filtered by season. If a `years` variable other than `ALL`was defined, then all data of the defined years are returned.
--- RFunction.R
+++ RFunction.R
@@ -1,14 +1,20 @@
-require('move')
+library('move2')
 require('foreach')
 library('lubridate')
 
-rFunction <- function(startTimestamp=NULL, endTimestamp=NULL, years='ALL', data)
+## The parameter "data" is reserved for the data object passed on from the previous app
+
+# to display messages to the user in the log file of the App in MoveApps
+# one can use the function from the logger.R file:
+# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+
+rFunction <- function(startTimestamp=NULL, endTimestamp=NULL, years="ALL",filter=TRUE, season=NULL, splitt=FALSE, data)
 {
   Sys.setenv(tz="UTC")
 
-  if (years=='ALL')
+  if (years=="ALL")
   {
-    years.vec <- unique(as.POSIXlt(timestamps(data),tz="UTC")$year+1900)
+    years.vec <- unique(as.POSIXlt(mt_time(data),tz="UTC")$year+1900)
     logger.info(paste0("You have selected all years of the data set: ",paste(years.vec,collapse=", ")))
   } else 
   {
@@ -19,7 +25,7 @@ rFunction <- function(startTimestamp=NULL, endTimestamp=NULL, years='ALL', data)
   if (is.null(startTimestamp) | is.null(endTimestamp))
   {
     logger.info("You did not provide a start and/or end timestamp. So it is not possible to filter for a season. The whole data set of the selected years is returned.")
-    if (years=='ALL') result <- data else result <- data[which((as.POSIXlt(timestamps(data))$year+1900) %in% years.vec),]
+    if (years=='ALL') result <- data else result <- data[which((as.POSIXlt(mt_times(data))$year+1900) %in% years.vec),]
   } else
   {
     startLT <- as.POSIXlt(startTimestamp,format="%Y-%m-%dT%H:%M:%OSZ",tz="UTC")
@@ -27,43 +33,82 @@ rFunction <- function(startTimestamp=NULL, endTimestamp=NULL, years='ALL', data)
     endLT <- as.POSIXlt(endTimestamp,format="%Y-%m-%dT%H:%M:%OSZ",tz="UTC")
     endday <- paste0(endLT$mon+1,"-",endLT$mday," ",endLT$hour,":",endLT$min,":",endLT$sec)
     
-    print(paste0("You have selected time between ",startday," and ",endday," of the years: ", years))
+    logger.info(paste0("You have selected time between ",startday," and ",endday," of the years: ", years))
+    
+    if (is.null(season))
+    {
+      logger.info(paste0("You have not provided a name for your season to appear in the annotaiton column. Based on your selected time range we set it to: ",startday," to ",endday,"."))
+      season <- paste(startday,"to",endday)
+    } else logger.info(paste0("You have provided that you season is called: ",season,". This name will appear in the annotation column."))
     
     starts <- strptime(paste0(years.vec,"-",startday),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC")
     ends <- strptime(paste0(years.vec,"-",endday),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC")
     len <- length(ends)
     
-    # adapt for ranges that cross NY
+    # adapt for ranges that cross NY, changed in 2024 to only add one year
     if (starts[1]>ends[1])
     {
-      starts <- c(strptime(paste0(years.vec[1],"-",1,"-",1," ",0,":",0,":",0),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC"),starts)
-      ends <- c(ends,strptime(paste0(years.vec[len],"-",12,"-",31," ",23,":",59,":",59),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC"))
+      #starts <- c(strptime(paste0(years.vec[1],"-",1,"-",1," ",0,":",0,":",0),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC"),starts)
+      #ends <- c(ends,strptime(paste0(years.vec[len],"-",12,"-",31," ",23,":",59,":",59),format=c("%Y-%m-%d %H:%M:%S"),tz="UTC"))
+      ends <- ends + years(1)
     }
     
     timeitvs <- data.frame("start"=as.POSIXct(starts,tz="UTC"), "end"=as.POSIXct(ends,tz="UTC"))
     timeitvs.list <- split(timeitvs, seq(nrow(timeitvs)))
     
-    data.split <- move::split(data)
+    if (!any(names(data)=="season")) data$season <- "none" #add column if it is not yet there
+    
+    data.split <- split(data, mt_track_id(data))
     filt <- foreach(datai = data.split) %:% 
       foreach(ti =timeitvs.list) %do% {
-        print(paste(namesIndiv(datai),":",ti$start,"-",ti$end))
-        datai[timestamps(datai)>=as.POSIXct(ti$start) & timestamps(datai)<=as.POSIXct(ti$end),]
-      }
+        logger.info(paste(unique(mt_track_id(datai)),":",ti$start,"-",ti$end))
+        ix <- which(mt_time(datai)>=as.POSIXct(ti$start) & mt_time(datai)<=as.POSIXct(ti$end))
+        if (any(ix)) datai$season[ix] <- season #added annotation
+        if (filter==TRUE) datai[mt_time(datai)>=as.POSIXct(ti$start) & mt_time(datai)<=as.POSIXct(ti$end),] else datai
+        }
     names(filt) <- names(data.split)
     
-    filt <-  setNames(lapply(seq_along(filt), function(x) {
-      yrs <- unlist(lapply(filt[[x]], function(y) year(timestamps(y)[1])))
-      setNames(filt[[x]],yrs)
-      }), names(filt))
-    
-    filt_nozero <- unlist(filt)[unlist(lapply(unlist(filt), length) > 0)] #allow move elements of length 1
-    
-    if (length(filt_nozero)==0) #if there remain no data at all
+    if (filter==TRUE)
     {
-      logger.info("!None of your data lie in the requested season. Reselect data set or time frame. Return NULL.") #moveStack does not allow empty objects
-      result <- NULL
-    } else result <- moveStack(filt_nozero)
+      if (splitt==FALSE)
+      {
+        logger.info("Your data will be filtered to the selected season, but not split to separate tracks. Note that this might lead to unrealistic steps.")
+        filt_jn <- foreach(filti = filt) %do% {
+          mt_stack(filt[[1]],.track_combine="merge")}
+        filtf <- filt_jn
+      }
+      if (splitt==TRUE)
+      {
+        logger.info("Your data will be filtered to the selected season and split to separate tracks per year. This will exclude unrealistic steps.")
+        filt <-  setNames(lapply(seq_along(filt), function(x) {
+          yrs <- unlist(lapply(filt[[x]], function(y) min(year(mt_time(y)[1]))))
+          setNames(filt[[x]],yrs)
+        }), names(filt))
+        
+        filt_spl <- unlist(filt,recursive=FALSE)
+        len_spl <- as.numeric(lapply(filt_spl,function(x) nrow(x)))
+        filt_spl_nn <- filt_spl[len_spl>0]
+        
+        filtf <- mt_stack(filt_spl_nn,.track_combine="rename") #the names should include the year, but this takes too much time now to figures out. later
+      }
+      
+      if (length(filtf)==0) #if there remain no data at all
+      {
+        logger.info("!None of your data lie in the requested season. Reselect data set or time frame. Return NULL.") #moveStack does not allow empty objects
+        result <- NULL
+      } else result <- filtf
+    } else #if want to only annotate season (above datai returned after each time interval)
+    {
+      logger.info("You have selected to not filter your tracks for the season, but only annotate them with an extra column indicating the selected season. The input tracks are returned with extra column.")
+      filt_jn <- foreach(filti = filt) %do% {
+        mt_stack(filt[[1]],.track_combine="merge")}
+      result <- filt_jn
+      if (splitt==TRUE) logger.info("You have selected to split your tracks by filtered season, but requested to NOT filter the tracks. That is not possible. If you want to split your tracks by season, select filter=TRUE. Now, the input tracks are returned with extra column.")
+    }
   }
 
   result
 }
+
+
+
